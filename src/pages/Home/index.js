62fa2d3c28d9618d44ceb3d60ed920153f36bab6
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-12-27 17:57:09 -0300

    Initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import './home.css';
+import apiFilmes from '../../services/api';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+import { Link } from 'react-router-dom';
+
+const Home = () =>  {
+
+  const [loading, setLoading] = useState(true);
+  const [filmes, setFilmes] = useState([]);
+  
+  const getFilmes = async () => {
+    await apiFilmes.get('r-api/?api=filmes')
+    .then((resp) => {
+      setFilmes(resp.data);
+      setLoading(false);
+    })
+    .catch((e) => console.log('error: ', e))
+    .finally(() => {})
+  }
+
+
+  useEffect(() => {
+    getFilmes();
+  }, [])
+
+  return (
+    <div className='container'>
+      <h1>Página Home</h1>
+      {!loading ? (
+        <div className='lista-filmes'>
+          {filmes.map((filme) => {
+            return(
+              <article key={filme.id}>
+                <strong>{filme.nome}</strong>
+                <img src={filme.foto} alt={filme.nome}/>
+                <Link to='/'>Acessar</Link>
+              </article>
+            );
+          })}
+        </div>
+      ) : (
+        <Loader
+          type="ThreeDots"
+          color="#c52626"
+          height={50}
+          width={50}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Home;
